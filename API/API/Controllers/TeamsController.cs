6632a8a6ebd0b74a1daa8cd3e3d91473cf053794
David <doriveramirez@gmail.com>
2020-03-10 17:43:07 +0000

    API

---
+++ API/API/Controllers/TeamsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using API.Models;
+
+namespace API.Controllers
+{
+    public class TeamsController : ApiController
+    {
+        private TestEntities db = new TestEntities();
+
+        // GET: api/Teams
+        public IQueryable<Team> GetTeam()
+        {
+            return db.Team;
+        }
+
+        // GET: api/Teams/5
+        [ResponseType(typeof(Team))]
+        public IHttpActionResult GetTeam(string id)
+        {
+            Team team = db.Team.Find(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(team);
+        }
+
+        // PUT: api/Teams/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTeam(string id, Team team)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != team.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(team).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TeamExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Teams
+        [ResponseType(typeof(Team))]
+        public IHttpActionResult PostTeam(Team team)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Team.Add(team);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (TeamExists(team.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = team.Id }, team);
+        }
+
+        // DELETE: api/Teams/5
+        [ResponseType(typeof(Team))]
+        public IHttpActionResult DeleteTeam(string id)
+        {
+            Team team = db.Team.Find(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            db.Team.Remove(team);
+            db.SaveChanges();
+
+            return Ok(team);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TeamExists(string id)
+        {
+            return db.Team.Count(e => e.Id == id) > 0;
+        }
+    }
+}
